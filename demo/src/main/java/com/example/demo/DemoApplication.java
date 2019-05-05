0db686bbff068dec1886ca4500f181e5e5bca857
tanwen <690387060@qq.com>
2019-05-05 08:28:40 +0800

    first commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
@@ -14,9 +16,16 @@ public class DemoApplication {
 	}
 	
 	@RequestMapping("hello")
+	@ResponseBody
 	public String TestWeb(){
 		return "Hello Word";
 	}
+	
+	@RequestMapping("index")
+    public String index(ModelMap map){
+        map.addAttribute("name","谈闻你好呐");
+        return "hello";
+    }
 	
 
 }
