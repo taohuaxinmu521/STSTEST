17c89fce897679dcb7ee2ab430883f5d201d38e3
tanwen <690387060@qq.com>
2019-05-22 07:57:14 +0800

    加入lombok, 需要在eclipse里安装插件

--- demo/src/main/java/com/example/demo/model/Girl.java
+++ demo/src/main/java/com/example/demo/model/Girl.java
@@ -1,5 +1,10 @@
 package com.example.demo.model;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
 public class Girl {
 	
 	private String id;
@@ -9,61 +14,5 @@ public class Girl {
 	private String cupSize;
 	
 	private String name;
-	
-	
-
-	public String getId() {
-		return id;
-	}
-
-
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-
-
-	public String getAge() {
-		return age;
-	}
-
-
-
-	public void setAge(String age) {
-		this.age = age;
-	}
-
-
-
-	public String getCupSize() {
-		return cupSize;
-	}
-
-
-
-	public void setCupSize(String cupSize) {
-		this.cupSize = cupSize;
-	}
-
-
-
-	public String getName() {
-		return name;
-	}
-
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "Girl [id=" + id + ", age=" + age + ", cupSize=" + cupSize + ", name=" + name + "]";
-	}
-	
 
 }
